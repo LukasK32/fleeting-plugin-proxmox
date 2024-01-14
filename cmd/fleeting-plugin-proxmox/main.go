d6aa280baead2c3719042c32724ff4f20b745e23
Lukas Kirylak <contact@lukask.pro>
2024-01-14 08:52:22 +0000

    build: Add GitLab CI

--- cmd/fleeting-plugin-proxmox/main.go
+++ cmd/fleeting-plugin-proxmox/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+)
+
+// Injected during CI build
+var (
+	VERSION = "dev"
+)
+
+var (
+	showVersion = flag.Bool("version", false, "Show version and exit")
 )
 
 func main() {
+	flag.Parse()
+	if *showVersion {
+		fmt.Println(VERSION)
+		os.Exit(0)
+	}
+
 	fmt.Println("Hello World!")
 }
